171cd0e606548d4a1ae5ad775abbb9407dbfd24a
Sabin Ghimire <sabinghimire223@gmail.com>
2025-07-06 22:01:04 +0545

    color effects

--- scripts.js
+++ scripts.js
@@ -27,8 +27,13 @@ function paintToCanvas() {
   return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height);
 
-    const pixels = ctx.getImageData(0, 0, width, height);
-    console.log(pixels);
+    let pixels = ctx.getImageData(0, 0, width, height);
+    // pixels = redEffects(pixels);
+    // pixels = rgbSplit(pixels);
+    // ctx.globalAlpha = 0.1;
+    pixels = greenScreen(pixels);
+    ctx.putImageData(pixels, 0, 0);
+    // console.log(pixels);
   }, 16);
 }
 
@@ -44,5 +49,55 @@ function takePhoto() {
   strip.insertBefore(link, strip.firstChild);
   // console.log(data);
 }
+
+function redEffects(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i + 0] = pixels.data[i + 0] + 100; // Red
+    pixels.data[i + 1] = pixels.data[i + 1] - 50; // Green
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // Blue
+  }
+  return pixels;
+}
+function rgbSplit(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i - 150] = pixels.data[i + 0]; // Red
+    pixels.data[i + 100] = pixels.data[i + 1]; // Green
+    pixels.data[i - 150] = pixels.data[i + 2]; // Blue
+  }
+  return pixels;
+}
+
+function greenScreen(pixels) {
+  const levels = {};
+  document.querySelectorAll(".rgb input").forEach((input) => {
+    levels[input.name] = input.value;
+  });
+
+  for (i = 0; i < pixels.data.length; i = i + 4) {
+    red = pixels.data[i + 0];
+    green = pixels.data[i + 1];
+    blue = pixels.data[i + 2];
+    alpha = pixels.data[i + 3];
+
+    if (
+      red >= levels.rmin &&
+      green >= levels.gmin &&
+      blue >= levels.bmin &&
+      red <= levels.rmax &&
+      green <= levels.gmax &&
+      blue <= levels.bmax
+    ) {
+      // take it out!
+      pixels.data[i + 3] = 0;
+    }
+  }
+
+  return pixels;
+}
+
+// Usage example:
+let pixels = ctx.getImageData(0, 0, canvas.width, canvas.height);
+pixels = greenScreen(pixels);
+ctx.putImageData(pixels, 0, 0);
 
 video.addEventListener("canplay", paintToCanvas);
