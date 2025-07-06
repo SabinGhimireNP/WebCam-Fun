0969e50968f7d6f5c66c43ab5254503e34cb3722
Sabin Ghimire <sabinghimire223@gmail.com>
2025-07-06 20:14:33 +0545

    Video visible

--- scripts.js
+++ scripts.js
@@ -1,5 +1,37 @@
-const video = document.querySelector('.player');
-const canvas = document.querySelector('.photo');
-const ctx = canvas.getContext('2d');
-const strip = document.querySelector('.strip');
-const snap = document.querySelector('.snap');
+const video = document.querySelector(".player");
+const canvas = document.querySelector(".photo");
+const ctx = canvas.getContext("2d");
+const strip = document.querySelector(".strip");
+const snap = document.querySelector(".snap");
+
+getVideo();
+
+function getVideo() {
+  navigator.mediaDevices
+    .getUserMedia({ video: true, audio: false })
+    .then((localMediaStream) => {
+      video.srcObject = localMediaStream;
+      video.play();
+    })
+    .catch((err) => {
+      console.error("Error accessing the camera:", err);
+    });
+}
+
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
+
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+  }, 16);
+}
+
+function TakePhoto() {
+  snap.currentTime = 0;
+  snap.play();
+}
+
+video.addEventListener("canplay", paintToCanvas);
